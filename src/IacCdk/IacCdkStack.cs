a9b6274af3a9e20824dbc1f69b67f5b36abfcd2a
Omar Ahmed <omarahmed1121@gmail.com>
2023-09-26 22:50:22 -0500

    Initial commit

---
+++ src/IacCdk/IacCdkStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace IacCdk
+{
+    public class IacCdkStack : Stack
+    {
+        internal IacCdkStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
