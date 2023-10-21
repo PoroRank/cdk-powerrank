cdbbb99124d0ef5ba96e3a50beae93958557a99f
Omar Ahmed <omarahmed1121@gmail.com>
2023-10-21 18:28:09 -0500

    add api gateway endpoints

--- src/IacCdk/IacCdkStack.cs
+++ src/IacCdk/IacCdkStack.cs
@@ -1,25 +1,19 @@
 using Amazon.CDK;
 using Amazon.CDK.AWS.APIGateway;
-using Amazon.CDK.AWS.CloudFront;
-using Amazon.CDK.AWS.CloudFront.Origins;
 using Amazon.CDK.AWS.DynamoDB;
 using Amazon.CDK.AWS.ECR;
-using Amazon.CDK.AWS.Events.Targets;
 using Amazon.CDK.AWS.Lambda;
-using Amazon.CDK.AWS.Lambda.EventSources;
 using Amazon.CDK.AWS.S3;
-using Amazon.CDK.AWS.SQS;
-using Amazon.CDK.AWS.WAFv2;
 using Constructs;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
-using static Amazon.CDK.AWS.WAFv2.CfnWebACL;
 
 namespace IacCdk
 {
     public class IacCdkStack : Stack
     {
+        const string SITEURL = "duzlyw7tx07x8.cloudfront.net/";
         internal IacCdkStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
         {
             #region "DynamoDB"
@@ -55,18 +49,6 @@ namespace IacCdk
             #endregion
 
             #region "ECR"
-            var dataCleanECR = new Repository(this, "data-cleaner", new RepositoryProps
-            {
-                RepositoryName = "data-cleaner",
-                AutoDeleteImages = true
-            });
-
-            var dataProcessECR = new Repository(this, "data-processor", new RepositoryProps
-            {
-                RepositoryName = "data-cleaner",
-                AutoDeleteImages = true
-            });
-
             var globalRankECR = new Repository(this, "get-global-rank", new RepositoryProps
             {
                 RepositoryName = "get-global-rank",
@@ -126,109 +108,36 @@ namespace IacCdk
             });
             #endregion
 
-            #region "S3"
-            var staticReactBucket = new Bucket(this, "StaticReactBucket");
-            #endregion
-
             #region "API Gateway"
             var apiGateway = new RestApi(this, "PowerRankApi", new RestApiProps
             {
                 RestApiName = "PowerRankApi",
-                Description = "PowerRank Lambda Powered Backend API",
+                Description = "PowerRank Serverless Backend API",
                 DefaultCorsPreflightOptions = new CorsOptions
                 {
                     AllowOrigins = new string[]
                     {
-                        staticReactBucket.BucketWebsiteUrl
+                        SITEURL
                     }
                 }
             });
 
-            var test2 = new SpecRestApi(this, "", new SpecRestApiProps
-            {
-                RestApiName = "",
-                ApiDefinition = ApiDefinition.FromAsset("")
-            });
+            var globalRankings = apiGateway.Root.AddResource("global_rankings");
+            globalRankings.AddMethod("GET", new LambdaIntegration(globalRankingFunction));
 
-            var test = new AssetApiDefinition("");
+            var tournamentRankings = apiGateway.Root.AddResource("tournament_rankings");
+            tournamentRankings.AddMethod("GET", new LambdaIntegration(tournamentRankingFunction));
 
+            var teamRankings = apiGateway.Root.AddResource("team_rankings");
+            teamRankings.AddMethod("GET", new LambdaIntegration(globalRankingFunction));
+
+            var tournaments = apiGateway.Root.AddResource("tournaments");
+            tournaments.AddMethod("GET", new LambdaIntegration(tournamentsFunction));
+
+            var teams = apiGateway.Root.AddResource("teams");
+            teams.AddMethod("GET", new LambdaIntegration(teamsFunction));
 
             #endregion
-
-            //#region "WAF"
-            ////ref: https://github.com/aws-samples/aws-cdk-examples/blob/master/csharp/CloudFront-S3-WAF/src/CdkStack.cs
-
-            ////Get the local machine Ip address.    
-            //var localIpAddress = GetIpAddressAsync().Result + "/32";
-
-            ////Restrict website access based on IP address by creating WAF Web ACL
-            //CfnIPSet cfnIPSet = new CfnIPSet(
-            //    this,
-            //    "AllowedIPs",
-            //    new CfnIPSetProps
-            //    {
-            //        Addresses = new string[] { localIpAddress }, //Provide list of allowed IP address. You can provide CIDR address as well.
-            //        IpAddressVersion = "IPV4",
-            //        Scope = "CLOUDFRONT"
-            //    }
-            //);
-
-            //CfnWebACL cfnWebACL = new CfnWebACL(
-            //    this,
-            //    "WebACL",
-            //    new CfnWebACLProps
-            //    {
-            //        DefaultAction = new DefaultActionProperty
-            //        {
-            //            Block = new BlockActionProperty
-            //            {
-            //                CustomResponse = new CustomResponseProperty { ResponseCode = 403 }
-            //            }
-            //        },
-            //        Scope = "CLOUDFRONT",
-            //        VisibilityConfig = new VisibilityConfigProperty
-            //        {
-            //            CloudWatchMetricsEnabled = true,
-            //            MetricName = "WebACLMetric",
-            //            SampledRequestsEnabled = true
-            //        },
-            //        Rules = new[]
-            //        {
-            //            new RuleProperty
-            //            {
-            //                Name = "WebACLRule",
-            //                Priority = 1,
-            //                Statement = new StatementProperty
-            //                {
-            //                    IpSetReferenceStatement = new IPSetReferenceStatementProperty
-            //                    {
-            //                        Arn = cfnIPSet.AttrArn
-            //                    }
-            //                },
-            //                VisibilityConfig = new VisibilityConfigProperty
-            //                {
-            //                    CloudWatchMetricsEnabled = true,
-            //                    MetricName = "WebACLRuleMetric",
-            //                    SampledRequestsEnabled = true
-            //                },
-            //                Action = new RuleActionProperty { Allow = new AllowActionProperty() }
-            //            }
-            //        }
-            //    }
-            //);
-            //#endregion
-
-            //#region "Cloudfront"
-            //var cloudFrontDistribution = new Distribution(this, "CloudFrontDistribution", new DistributionProps
-            //{
-            //    DefaultBehavior = new BehaviorOptions
-            //    {
-            //        Origin = new S3Origin(staticReactBucket)
-            //    },
-            //    WebAclId = cfnWebACL.AttrArn,
-            //    PriceClass = PriceClass.PRICE_CLASS_100
-            //});
-            //#endregion
 
             #region "Permissions"
             rankingTable.GrantReadData(globalRankingFunction);
@@ -238,14 +147,6 @@ namespace IacCdk
             #endregion
 
             #region "Output"
-            //new CfnOutput(this, "CloudFront URL", new CfnOutputProps
-            //{
-            //    Value = string.Format(
-            //        "https://{0}/index.html",
-            //        cloudFrontDistribution.DomainName
-            //        )
-            //});
-
             new CfnOutput(this, "APi Gateway URL", new CfnOutputProps
             {
                 Value = apiGateway.Url
